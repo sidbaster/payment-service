00fe009c30d15c83276823e543b621ee27546e23
still <stillwaitingsv@gmail.com>
2025-09-21 14:56:24 +0200

    homework-4. Step 15. Fix checkstyle error

--- payment-system/src/main/java/com/payment/system/controller/PaymentController.java
+++ payment-system/src/main/java/com/payment/system/controller/PaymentController.java
@@ -27,8 +27,6 @@ public class PaymentController {
         return ResponseEntity.ok(paymentService.getPaymentById(id));
     }
 
-    public static void fetregsdgkdsfjgdkflgjdfgkdfgjdfkglsdjfgklgjsdfglkjdfglksdfgjklfkglfhdsghdfjdfkgdfjhgsfshdfsdgfhjkasdfhgshdjakfgkhajsdhgfsdkjfgasdjfhgsdkjahfgkjasdhfgjaksdghjfgsdfjglkdsfjgklsdfjghdfjhghkjgjhldfsklgdfg(){}
-
     @GetMapping
     public ResponseEntity<List<Payment>> getPayments() {
         return  ResponseEntity.ok(paymentService.getPayments());
